cc897ff024943117023322e0a1918a79c764b43f
Paola Garcia <garciaamezola@gmail.com>
2015-02-05 07:07:46 +0000

    the improved version was changed to be able to set significant figures and treat lower case sequences to do this we added the round() function and converted the sequence to upper case before starting the calculation using upper()

--- improved_function.py
+++ improved_function.py
@@ -1,6 +1,17 @@
+from __future__ import division
 def get_at_content(dna):
     length = len(dna)
-    a_count = dna.count('A')
-    t_count = dna.count('T')
+#to solve the lower case problem, convert the input sequence to upper case before starting the calculation
+    a_count = dna.upper() .count('A')
+    t_count = dna.upper() .count('T')
     at_content = (a_count + t_count) / length
-    return at_content
+#the round function takes the argument you want to round and the number of sig. figs. you want.
+    return round(at_content, 2)
+
+my_at_content = get_at_content("ATGCGCGATCGATCGAATCG")
+print(str(my_at_content))
+print(get_at_content("ATGCGCGATCGATCGAATCG"))
+print(get_at_content("aactgtagctagctagcagcgta"))
+
+
+
